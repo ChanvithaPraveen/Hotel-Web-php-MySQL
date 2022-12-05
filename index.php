f3af5e5d19b094f372f0707dd99e4f69d93c9630
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 23:19:05 +0530

    Created image cards and top topics sections

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <!-- This is the content of the page -->
     <section class="main">
 
-    <!-- ******************************* Navigation Bar ******************************* -->
+        <!-- ******************************* Navigation Bar ******************************* -->
 
         <!-- Navigation bar section -->
         <div class="nav-section">
@@ -42,30 +42,112 @@
         <!-- *******************************Image slide show ******************************* -->
 
         <div class="slideshow-container">
-
             <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
-                <img src="img1.jpg" style="width:100%">
-                <div class="text">Caption Text</div>
+                <img src="images/img1.jpg" style="width:100%">
+                <div class="text">Welcome to Sri Lanka</div>
             </div>
-
             <div class="mySlides fade">
                 <div class="numbertext">2 / 3</div>
-                <img src="img2.jpg" style="width:100%">
-                <div class="text">Caption Two</div>
+                <img src="images/img2.jpg" style="width:100%;">
+                <div class="text">Hotel Asia !</div>
             </div>
-
             <div class="mySlides fade">
                 <div class="numbertext">3 / 3</div>
-                <img src="img3.jpg" style="width:100%">
-                <div class="text">Caption Three</div>
+                <img src="images/img3.jpg" style="width:100%">
+                <div class="text">About Us</div>
             </div>
-
+            <a class="prev" onclick="plusSlides(-1)">❮</a>
+            <a class="next" onclick="plusSlides(1)">❯</a>
         </div>
         <br>
-
         <div style="text-align:center">
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
         </div>
+
+
+        <!-- ******************************* Hotel Asia Body ******************************* -->
+
+        <div>
+            <div class="top-topics">
+                <h1>Hotel Asia</h1>
+                <p>Hotel Asia is a 3-star hotel located in the heart of Colombo, Sri Lanka. It is a 5-minute walk from
+                    the famous Galle Face Green and the National Museum. The hotel is 1.5 km from the Colombo Fort
+                    Railway Station and 1.6 km from the Colombo National Hospital. The Bandaranaike International
+                    Airport is 35 km away. The hotel offers free WiFi in all areas. The rooms are equipped with a
+                    flat-screen TV with satellite channels, a kettle, a shower, a hairdryer and a desk. The rooms are
+                    equipped with a private bathroom with a bath. All rooms have a wardrobe. The hotel offers a 24-hour
+                    front desk, room service and currency exchange for guests. The hotel also offers car rental. The
+                    hotel has a restaurant. The nearest airport is Bandaranaike International Airport, 35 km from Hotel
+                    Asia.</p>
+            </div>
+
+            <hr>
+
+            <div class="top-topics">
+                <h1>Exclusive Offers</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatibus at assumenda, unde
+                    error, illum laudantium, perferendis sed aut asperiores quisquam qui voluptate voluptatum suscipit
+                    quis expedita doloribus quaerat non?</p>
+            </div>
+        </div>
+
+        <!-- ******************************* Image Cards ******************************* -->
+
+        <div class="card">
+            <div class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg">
+            </div>
+            <div class="title">
+                <h1>Write title Here</h1>
+            </div>
+            <div class="des">
+                <p>You can Add Desccription Here...</p>
+                <button>Read More...</button>
+            </div>
+        </div>
+        <!--cards -->
+
+        <div class="card">
+            <div class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg">
+            </div>
+            <div class="title">
+                <h1>Write title Here</h1>
+            </div>
+            <div class="des">
+                <p>You can Add Desccription Here...</p>
+                <button>Read More...</button>
+            </div>
+        </div>
+        <!--cards -->
+
+        <div class="card">
+            <div class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg">
+            </div>
+            <div class="title">
+                <h1>Write title Here</h1>
+            </div>
+            <div class="des">
+                <p>You can Add Desccription Here...</p>
+                <button>Read More...</button>
+            </div>
+        </div>
+        <!--cards -->
+
+        <div class="card">
+            <div class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg">
+            </div>
+            <div class="title">
+                <h1>Write title Here</h1>
+            </div>
+            <div class="des">
+                <p>You can Add Desccription Here...</p>
+                <button>Read More...</button>
+            </div>
+        </div>
+        <!--cards -->
