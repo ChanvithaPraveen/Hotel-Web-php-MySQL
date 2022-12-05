1b0376cadd260a7c66ba631651b28245a945b7d4
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 08:43:39 +0530

    Created html block

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>My first three.js app</title>
-    <style>
-        body { margin: 0; }
-        canvas { width: 100%; height: 100% }
-    </style>
-</head>
-<body>
-    <php
-        $servername = "localhost";
+    <title>My Page</title>
 
-     ?>
+    
+</html>
