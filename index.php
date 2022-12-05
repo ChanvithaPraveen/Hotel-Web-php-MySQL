7d181fad60f73c7a58f3b8ee6da07b39725b52b9
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 11:16:59 +0530

    css

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Page</title>
+    <link rel="stylesheet" href="css/styles.css"/>
+</head>
+
 <body>
-    <h1>Hello lllllllllllll</h1>
-    <h2>ggggggggggggg</h2>
-    <p>
-        <?php
-            echo "Hello World";
-            for ($i = 0; $i < 10; $i++) {
-                echo $i;
-            }
-        ?>
-    </p>
+    <!-- This is the content of the page -->
+    <section class="main">
+        <nav>
+            <ul>
+                <a href="#" class="logo">
+                    <img src="images/4.jpg" alt="logo">
+                </a>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </section>
 </body>
 
 </html>
