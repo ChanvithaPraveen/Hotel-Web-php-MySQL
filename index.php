3a0642b68da8192118e11927bcc3a2e24b7b11df
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 12:13:34 +0530

    Change image

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
                 </a>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
+                <li><a href="gallery.php">Gallery</a></li>
+                <li><a href="offers.php">Offers</a></li>
+                <li><a href="reservations.php">Reservations</a></li>
+                <li><a href="location.php">Location</a></li>
                 <li><a href="contact.php">Contact</a></li>
             </ul>
         </nav>
