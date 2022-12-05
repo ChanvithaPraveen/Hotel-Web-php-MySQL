4fc10b538c95142873a35f79857fe1b7a17c95b5
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 11:16:25 +0530

    added css body components

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 <html>
 <head>
     <title>My Page</title>
+<body>
+    <h1>Hello lllllllllllll</h1>
+    <h2>ggggggggggggg</h2>
+    <p>
+        <?php
+            echo "Hello World";
+            for ($i = 0; $i < 10; $i++) {
+                echo $i;
+            }
+        ?>
+    </p>
+</body>
 
-    
 </html>
