cc4d33b133a8fb30ed8137c7d4d5fee9068612a9
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 16:09:48 +0530

    Created functions js file and slidesow functions

--- index.php
+++ index.php
@@ -5,26 +5,76 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Page</title>
-    <link rel="stylesheet" href="css/styles.css"/>
+    <link rel="stylesheet" href="css/styles.css" />
+    
 </head>
 
 <body>
+<script src="js/functions.js"></script>
     <!-- This is the content of the page -->
     <section class="main">
-        <nav>
-            <ul>
-                <a href="#" class="logo">
-                    <img src="images/4.jpg" alt="logo">
-                </a>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="gallery.php">Gallery</a></li>
-                <li><a href="offers.php">Offers</a></li>
-                <li><a href="reservations.php">Reservations</a></li>
-                <li><a href="location.php">Location</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
+
+
+        <!-- Navigation bar section -->
+        <div class="nav-section">
+            <!-- logo -->
+            <a href="#" class="logo">
+                <img src="images/4.jpg" alt="logo">
+            </a>
+
+            <!-- navigation links -->
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="offers.php">Offers</a></li>
+                    <li><a href="reservations.php">Reservations</a></li>
+                    <li><a href="location.php">Location</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+
+
+
+        <!-- Slideshow container -->
+        <div class="slideshow-container">
+
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="images/img1.jpg" style="width:100%">
+                <div class="text">Caption Text</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 3</div>
+                <img src="images/img2.jpg" style="width:100%">
+                <div class="text">Caption Two</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 3</div>
+                <img src="images/img3.jpg" style="width:100%">
+                <div class="text">Caption Three</div>
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
+
+        <!-- The dots/circles -->
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+
+
+
     </section>
 </body>
 
