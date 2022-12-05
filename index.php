0706ffd343e770f0848aa020f83b784e233bf81c
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-12-05 20:55:40 +0530

    Change the slideshow components

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Page</title>
     <link rel="stylesheet" href="css/styles.css" />
-    
+
 </head>
 
 <body>
-<script src="js/functions.js"></script>
+    <script src="js/functions.js"></script>
+
     <!-- This is the content of the page -->
     <section class="main">
 
+    <!-- ******************************* Navigation Bar ******************************* -->
 
         <!-- Navigation bar section -->
         <div class="nav-section">
@@ -37,45 +39,33 @@
         </div>
 
 
+        <!-- *******************************Image slide show ******************************* -->
 
-        <!-- Slideshow container -->
         <div class="slideshow-container">
 
-            <!-- Full-width images with number and caption text -->
             <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
-                <img src="images/img1.jpg" style="width:100%">
+                <img src="img1.jpg" style="width:100%">
                 <div class="text">Caption Text</div>
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">2 / 3</div>
-                <img src="images/img2.jpg" style="width:100%">
+                <img src="img2.jpg" style="width:100%">
                 <div class="text">Caption Two</div>
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">3 / 3</div>
-                <img src="images/img3.jpg" style="width:100%">
+                <img src="img3.jpg" style="width:100%">
                 <div class="text">Caption Three</div>
             </div>
 
-            <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
         <br>
 
-        <!-- The dots/circles -->
         <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
         </div>
-
-
-
-    </section>
-</body>
-
-</html>
